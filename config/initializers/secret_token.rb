7f127996f0d135a6742e0420f76bd17cfb5a7f62
Sebastian <sebastian.prokesch@gmail.com>
2012-05-15 14:53:49 +0200

    start Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IRTPad::Application.config.secret_token = '4030be341e39975b5e3cf4e4f80912c338e25bc4fd65dfe1bf321942d3642c58a4aabb33050ae31e9d44bae17525295aeca5ab336f0b2eed13e5b53506a25cd9'
